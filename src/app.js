3ecad1d0a8ea36b4758de66c5e4ac38c52e01b07
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-03-31 18:40:10 -0400

    Bot js added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-import { Client } from 'discord.js'
+import { basename, Client } from 'discord.js'
+import { configDotenv } from 'dotenv'
+
+configDotenv()
+
+const { BRD_BOT_TOKEN } = process.env;
+console.log(BRD_BOT_TOKEN)
 
 const client = new Client({ intents: 3276799 })
-const BRD_BOT_ALL_THING = 'MTEyMjYyMjE3NDIyMjYzNTExMA.GQnrZT.GTAB5zsA7q5m8kSg5bpSVYQcrV5bqPWUdM8E9s'
+
 
 client.on('ready', () => {
     console.log(`BRD | BOT'S READY... ${client.user.tag}`)
 })
 
-client.login(BRD_BOT_ALL_THING);
+client.login(BRD_BOT_TOKEN);
